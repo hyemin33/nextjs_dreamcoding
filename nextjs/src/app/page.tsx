d2014fc6f142039b1993626fa8f9cf2c8021b920
hyemin33 <joliie@naver.com>
2024-02-04 18:31:24 +0900

    클라이언트 컴포넌트 만들기

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,3 +1,11 @@
+import Counter from "@/components/Counter";
+
 export default function Home() {
-  return <h1>홈페이지다!</h1>;
+  return (
+    <>
+      <h1>홈페이지다!</h1>
+
+      <Counter />
+    </>
+  );
 }
